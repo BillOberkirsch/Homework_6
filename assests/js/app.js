246255c292d632f4e852f97ec0c1f686d3e4bf9d
BillOberkirsch <83283761+BillOberkirsch@users.noreply.github.com>
2021-12-04 10:50:59 -0500

    added in the ability to clear forcast, clear localstorage and all controls.

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,10 +1,12 @@
 // Global variables
-var searchHistory = [];
+var searchCities = [];
 var currentCity;
-var lParentDiv = document.getElementById('rightDiv');
-//var uvi;
+var rParentDiv = document.getElementById('rightDiv');//right parent div
+
 var requestUrl = "http://api.openweathermap.org/data/2.5/forecast";
-var repoList = document.querySelector('ul');
+var btnFrmEl = document.getElementById('btn-Form');
+
+
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
@@ -23,16 +25,15 @@ function handleSubmit(event)
 {
     event.preventDefault();
 
-    console.log('City to look up: ', txtSearch.val());
     currentCity = txtSearch.val();
     getApi(currentCity);
+    save_ToStorage();
 }
 /*Me -- Mine*/
 function getApi(currentCity)
 {
-    console.log("I am in getApi and current city is: " + currentCity);
     var url = requestUrl +"?q=" + currentCity + "&units=imperial&appid=" + apiKey;
-     console.log("the url is: " + url);
+    console.log(url);
     //Used postman to test and verify api call.  Used it functionality to convert to JavaScript-fetch
     //then modified it for my needs.  Also used project 4 of section 6.
 
@@ -43,6 +44,7 @@ function getApi(currentCity)
         })
         .then(function(data)
         {
+            removeAllChildNodes(rParentDiv);
             display_Weather(data);
         })
        .catch(error => console.log('error', error));
@@ -57,9 +59,8 @@ function display_Weather(dataObj)
     var maxTemp;
     var feels_like;
     var cityName = dataObj.city.name;
-    console.log("CityName:" + cityName);
     var List = dataObj.list;
-    console.log(List);
+    
     for(let i = 0; i < List.length; i+=8)
     {
         day = List[i].dt_txt.slice(0, -9);
@@ -68,12 +69,12 @@ function display_Weather(dataObj)
         minTemp = List[i].main.temp_min;
         maxTemp = List[i].main.temp_max;
         feels_like = List[i].main.feels_like;
-        create_card(day,cloud,currentTemp,minTemp, maxTemp, feels_like);
+        create_card(day,cloud,currentTemp,minTemp, maxTemp, feels_like, cityName);
     }
 }
 
 /*Me -- Mine*/
-function create_card(day,cloud,ctemp,mintemp,maxtemp,flike)
+function create_card(day,cloud,ctemp, mintemp, maxtemp, flike, cityName)
 {
     var newCard = document.createElement("div");
     newCard.className = "card";
@@ -86,7 +87,11 @@ function create_card(day,cloud,ctemp,mintemp,maxtemp,flike)
     cardBody.className="card-body";
 
     var cardLbl = document.createElement("label");
-    cardLbl.textContent = "Cloud Cover: " + cloud + "%";
+    cardLbl.textContent = "For the city of " + cityName;
+    cardBody.appendChild(cardLbl);
+
+    cardLbl = document.createElement("label");
+    cardLbl.textContent= "Cloud Cover: " + cloud + "%";
     cardBody.appendChild(cardLbl);
 
     cardLbl = document.createElement("label");
@@ -109,8 +114,92 @@ function create_card(day,cloud,ctemp,mintemp,maxtemp,flike)
     newCard.appendChild(cardBody)
 
 
-    lParentDiv.append(newCard);
+    rParentDiv.append(newCard);
 }
 
+//javascript tutorial https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+function removeAllChildNodes(parent)
+{
+    console.log(parent);
+    while (parent.firstChild) 
+    {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function save_ToStorage()
+{
+     if(!searchCities.includes(currentCity))//w3schools.com
+     {
+         searchCities.push(currentCity);
+        localStorage.setItem("searchCities", JSON.stringify(searchCities));
+        console.log("search cities: " + searchCities);
+        show_Cities();
+     }
+    console.log("IN save_ToStorage, current city is:" + currentCity);
+    
+}
+
+function show_Cities()
+{
+    removeAllChildNodes(btnFrmEl);
+    if(searchCities.length !==0)
+    {
+        for(var i = 0; i < searchCities.length; i++)
+        {
+            if(searchCities[i] !== '')
+            {
+                var btEl = document.createElement('button');
+                btEl.textContent =searchCities[i];
+                btEl.classList="btn btn-success"
+                btEl.type = "button";
+                btEl.addEventListener("click", get_city);
+                btnFrmEl.appendChild(btEl);
+            }
+        }
+    }
+}
+
+function get_city(event)
+{
+    event.preventDefault();
+    currentCity = event.target.textContent;
+    getApi(currentCity);
+    //txtSearch.val() = currentCity;
+}
+
+function get_Cities()
+{
+    var s_cities = JSON.parse(localStorage.getItem("searchCities"));
+    if(s_cities !== null)
+    {
+        searchCities = s_cities;
+        console.log(searchCities);
+        show_Cities(searchCities);
+    }
+}
+
+function Clear()
+{
+    console.log("hello in clear!");
+    removeAllChildNodes(rParentDiv);
+
+}
+function Clear_All()
+{
+    localStorage.clear();
+    removeAllChildNodes(rParentDiv);
+    removeAllChildNodes(btnFrmEl);
+
+}
 
 formEl.on('submit', handleSubmit);
+
+
+function init()
+{
+    //localStorage.clear();
+    get_Cities();
+}
+
+init();
